d90e8debaac28e666022bdbdf8c4c306a1d929ea
marcosdevelo <marcosgomezda@gmail.com>
2019-07-17 01:05:53 +0000

    marcosdevelo

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload= () =>{
+
+let arrayNum=['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+ let arrSym=['\u2660','\u2663','\u2665','\u2666'];
+let randomNum = arrayNum[Math.floor(Math.random() * arrayNum.length)];
+	let randomSuit = arrSym[Math.floor(Math.random() * arrSym.length)];
+if(item===\u2665){
+
+}
+
+
+
+
+
+
+
+    document.querySelector('#number').innerHTML= randomNum;
+    document.querySelector('#top').innerHTML= randomSuit;
+     document.querySelector('#bottom').innerHTML= randomSuit;
+};
