5b74f7a2b3d1044a611b3c13a9757f62801e3ffc
marcosdevelo <marcosgomezda@gmail.com>
2019-07-18 22:37:01 +0000

    marcosdevelo

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let arrayNum=['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
  let arrSym=['\u2660','\u2663','\u2665','\u2666'];
 let randomNum = arrayNum[Math.floor(Math.random() * arrayNum.length)];
 	let randomSuit = arrSym[Math.floor(Math.random() * arrSym.length)];
-if(item===\u2665){
+if(randomSuit==='\u2665'|| randomSuit==='\u2666'){
+
+    document.querySelector('#top').className= "top-suit red";
+     document.querySelector('#bottom').className= "bottom-suit red";
 
 }
 
